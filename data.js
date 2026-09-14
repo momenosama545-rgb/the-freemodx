c634783e2fbbcb0cbeea0aeaf4e09c9b8ee851e9
momenosama545-rgb <momenosama545@gmail.com>
2026-09-14 17:06:27 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,50 +1,41 @@
 const appsData = [
-    {
-        id: "pubg-zonex-freemodx",
-        name: "PUBG Mobile (ZoneX & FreeModX)",
-        category: "configs",
-        version: "4.6",
-        size: "3.6 MB",
-        features: "ESP, Aimbot, 165 FPS, iPad View",
-        description: "تحديث 4.6 بدون بان - كشف أماكن دقيق، إيم بوت، 165 فريم، منظور إيباد - تعاون حصري بين ZoneX و FreeModX.",
-        downloadLink: "https://www.mediafire.com/file/m7ysrbzmmgenz76/تعاون+zonex+و+freemodx.zip/file",
-        videoEmbed: "https://www.youtube.com/embed/B0pi7GawzYg",
-        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/ima%D8%A8%D8%B1%D9%8A%D9%84%D9%8Ages.jpg?raw=true" 
-    },
-    {
-        id: "8bp-expert",
-        name: "8 Ball Pool Mod",
-        category: "games",
-        version: "5.10.0",
-        size: "201 MB",
-        features: "Extended Guideline, Anti-Ban",
-        description: "نسخة مهكرة من لعبة 8 Ball Pool مع خط إرشاد طويل ومميزات حصرية.",
-        downloadLink: "https://www.mediafire.com/file/d71tf64mo4t2zl5/8+Ball+Pool_56.18.2_sign.apk/file",
-        videoEmbed: "",
-        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/Gemini_Generated_Image_rcabrcabrcabrcab.jpg?raw=true"
-    },
-    {
-        id: "snaptube-pro",
-        name: "Snaptube VIP",
-        category: "apps",
-        version: "7.0.2",
-        size: "25 MB",
-        features: "Ad-Free, HD Downloads",
-        description: "تطبيق لتحميل الفيديوهات والأغاني من يوتيوب ومختلف منصات التواصل الاجتماعي بدون إعلانات.",
-        downloadLink: "https://www.mediafire.com/file/m3i6zed01oplulb/Snaptube+v7.66.1.76672501+(Premium).apk/file",
-        videoEmbed: "",
-        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/Gemini_Generated_Image_nqnk8knqnk8knqnk.jpg?raw=true"
-    },
-    {
-        id: "truecaller-pro",
-        name: "Truecaller Gold / Premium",
-        category: "apps",
-        version: "26.27.5",
-        size: "105 MB",
-        features: "Caller ID, Spam Blocking, Gold Badge",
-        description: "تطبيق تروكولر لمعرفة اسم المتصل، حظر المكالمات المزعجة، وتفعيل مميزات الحساب الذهبي والمدفوع.",
-        downloadLink: "https://www.mediafire.com/file/f21oot7692qumni/Truecaller_26.27.5_12627005_3b3b92.apk/file",
-        videoEmbed: "",
-        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/Gemini_Generated_Image_p8ccjcp8ccjcp8cc.jpg?raw=true"
-    }
+    {
+        id: "pubg-zonex-freemodx",
+        name: "PUBG Mobile (ZoneX & FreeModX)",
+        category: "configs",
+        version: "4.6",
+        size: "3.6 MB",
+        features: "ESP, Aimbot, 165 FPS, iPad View",
+        description: "تحديث 4.6 بدون بان - كشف أماكن دقيق، إيم بوت، 165 فريم، منظور إيباد - تعاون حصري بين ZoneX و FreeModX.",
+        downloadLink: "https://www.mediafire.com/file/m7ysrbzmmgenz76/تعاون+zonex+و+freemodx.zip/file",
+        videoEmbed: "https://www.youtube.com/embed/B0pi7GawzYg",
+        // حط هنا رابط صورة لـ ببجي أو شعار التعاون
+        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/ima%D8%A8%D8%B1%D9%8A%D9%84%D9%8Ages.jpg?raw=true" 
+    },
+    {
+        id: "8bp-expert",
+        name: "8 Ball Pool Mod",
+        category: "games",
+        version: "5.10.0",
+        size: "201 MB",
+        features: "Extended Guideline, Anti-Ban",
+        description: "نسخة مهكرة من لعبة 8 Ball Pool مع خط إرشاد طويل ومميزات حصرية.",
+        downloadLink: "https://www.mediafire.com/file/d71tf64mo4t2zl5/8+Ball+Pool_56.18.2_sign.apk/file",
+        videoEmbed: "",
+        // رابط صورة لعبة 8 Ball Pool
+        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/Gemini_Generated_Image_rcabrcabrcabrcab.jpg?raw=true"
+    },
+    {
+        id: "snaptube-pro",
+        name: "Snaptube VIP",
+        category: "apps",
+        version: "7.0.2",
+        size: "25 MB",
+        features: "Ad-Free, HD Downloads",
+        description: "تطبيق لتحميل الفيديوهات والأغاني من يوتيوب ومختلف منصات التواصل الاجتماعي بدون إعلانات.",
+        downloadLink: "https://www.mediafire.com/file/m3i6zed01oplulb/Snaptube+v7.66.1.76672501+(Premium).apk/file",
+        videoEmbed: "",
+        // رابط صورة تطبيق سناب تيوب
+        icon: "https://github.com/momenosama545-rgb/the-freemodx/blob/main/Gemini_Generated_Image_nqnk8knqnk8knqnk.jpg?raw=true"
+    }
 ];
